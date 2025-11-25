e216483561434ebf4ec2009a80a1f2fb51602eed
Brian Espina <espinabrian@gmail.com>
2025-11-25 16:00:34 +0800

    hero section updates.

--- app/header.tsx
+++ app/header.tsx
@@ -4,13 +4,13 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="w-full absolute z-20">
+    <header className="w-full absolute z-20 bg-linear-to-b from-black/50 to-transparent">
       <div className="bg-gray-950 text-(--color-light) border-b-3 border-b-(--color-theme)">
         <div className="container p-2 m-auto flex flex-row justify-between">
           <div>
             <Link className="flex flex-row gap-2 items-center" href="/"><PhoneCall size={18} />Call us now</Link>
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-5">
             <Facebook size={20} />
             <Twitter size={20} />
             <Instagram size={20} />
@@ -19,14 +19,13 @@ export default function Header() {
       </div>
       <div className="container m-auto flex flex-row justify-between items-center h-full p-4 pt-6">
         <Image
-          src="/healthy-smiles-v1.png"
+          src="/healthy-smiles-v2.png"
           width={150}
           height={100}
           alt="Dentist Logo"
-          className="brightness-0 invert"
         />
         <nav className="flex flex-row gap-7 items-center">
-          <ul className="flex flex-row gap-7">
+          <ul className="flex flex-row gap-7 theme-text-shadow">
             <li>
               <Link href="/" className="text-white border-b-2 border-(--color-theme) pb-2 font-bold">Home</Link >
             </li>
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <section className="h-full bg-cover relative flex items-center pb-52 pt-72 bg-center" style={{
+      <section className="h-full bg-cover relative flex items-center pb-52 pt-64 object-cover object-right" style={{
         backgroundImage:
-          "url(/hero-section.jpg)",
+          "url(/hero-section-6.jpg)",
       }}>
         <div className="container p-4 z-20 m-auto">
           <div className="flex flex-col gap-4 w-7/12">
             <h1>Modern Dental Care for Healthy, Confident Smiles</h1>
-            <p className="text-white">Experience gentle, precise, and patient-focused treatments using advanced dental technology.</p>
+            <p className="text-white/90 theme-text-shadow">Experience gentle, precise, and patient-focused treatments using advanced dental technology.</p>
             <div className="flex flex-row gap-2 pt-10">
               <Link className="button bg-(--color-theme) text-white" href="/">Book an Appointment</Link>
               <Link className="button border border-white text-white" href="/">Call Us</Link>
@@ -19,11 +19,11 @@ export default function Home() {
             <small className="text-(--color-light) flex flex-row items-center gap-2"><CalendarClock size={20} /> Open Monday–Saturday. Walk-ins welcome.</small>
           </div>
         </div>
-        <div className="absolute inset-0 bg-linear-to-r from-black/95 to-black/40 z-10"></div>
+        <div className="absolute inset-0 bg-linear-to-r from-black/40 transparent z-10"></div>
       </section>
       <section>
         <div className="container px-4 py-20 m-auto">
-          <h2>Why Patients Trust Our Dental Care</h2>
+          <h2 className="text-center">Why Patients Trust Our Dental Care</h2>
         </div>
       </section>
     </main>
