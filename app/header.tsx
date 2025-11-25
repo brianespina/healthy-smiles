f4999c65852fc62806cf691d4214156922164ef8
Brian Espina <espinabrian@gmail.com>
2025-11-25 20:11:02 +0800

    added about page

--- app/header.tsx
+++ app/header.tsx
@@ -1,21 +1,13 @@
-import { Facebook, Instagram, PhoneCall, Twitter } from "lucide-react"
+import { PhoneCall } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
 export default function Header() {
   return (
     <header className="w-full absolute z-20 bg-linear-to-b from-black/50 to-transparent">
-      <div className="bg-gray-950 text-(--color-light) border-b-3 border-b-(--color-secondary)">
-        <div className="container p-2 m-auto flex flex-row justify-between">
-          <div>
-            <Link className="flex flex-row gap-2 items-center" href="/"><PhoneCall size={18} />Call us now</Link>
-          </div>
-          <div className="flex flex-row gap-5">
-            <Facebook size={20} />
-            <Twitter size={20} />
-            <Instagram size={20} />
-          </div>
-        </div>
+      <div className="w-full bg-gray-950 border-b-3 border-(--color-secondary) text-white text-sm py-2 flex justify-center items-center gap-2">
+        <PhoneCall size={16} className="opacity-90" />
+        <span className="tracking-wide opacity-75">Have questions? Our team is here to help. Reach us at (123) 456-7890.</span>
       </div>
       <div className="container m-auto flex flex-row justify-between items-center h-full p-4 pt-6">
         <Image
@@ -30,7 +22,7 @@ export default function Header() {
               <Link href="/" className="text-white border-b-2 border-(--color-theme) pb-2 font-bold">Home</Link >
             </li>
             <li>
-              <Link href="/" className="text-white">About</Link >
+              <Link href="/about" className="text-white">About</Link >
             </li>
             <li>
               <Link href="/" className="text-white">Gallery</Link >
