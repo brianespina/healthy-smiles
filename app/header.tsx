5432dc849ae6ff0a1f93d626fd9a20701d1c1f99
Brian Espina <espinabrian@gmail.com>
2025-11-25 14:14:47 +0800

    hero section added

---
+++ app/header.tsx
@@ -0,0 +1,48 @@
+import { Facebook, Instagram, PhoneCall, Twitter } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <header className="w-full absolute z-20">
+      <div className="bg-gray-950 text-(--color-light) border-b-3 border-b-(--color-theme)">
+        <div className="container p-2 m-auto flex flex-row justify-between">
+          <div>
+            <Link className="flex flex-row gap-2 items-center" href="/"><PhoneCall size={18} />Call us now</Link>
+          </div>
+          <div className="flex flex-row gap-2">
+            <Facebook size={20} />
+            <Twitter size={20} />
+            <Instagram size={20} />
+          </div>
+        </div>
+      </div>
+      <div className="container m-auto flex flex-row justify-between items-center h-full p-4 pt-6">
+        <Image
+          src="/healthy-smiles-v1.png"
+          width={150}
+          height={100}
+          alt="Dentist Logo"
+          className="brightness-0 invert"
+        />
+        <nav className="flex flex-row gap-7 items-center">
+          <ul className="flex flex-row gap-7">
+            <li>
+              <Link href="/" className="text-white border-b-2 border-(--color-theme) pb-2 font-bold">Home</Link >
+            </li>
+            <li>
+              <Link href="/" className="text-white">About</Link >
+            </li>
+            <li>
+              <Link href="/" className="text-white">Gallery</Link >
+            </li>
+            <li>
+              <Link href="/" className="text-white">FAQ</Link >
+            </li>
+          </ul>
+          <Link className="bg-(--color-theme) text-(--color-light) button hover:bg-gray-800 transition-colors" href="/contact">Contact Us</Link>
+        </nav>
+      </div>
+    </header >
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
-import Image from "next/image";
+import { CalendarClock } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-      </main>
-    </div>
+    <main>
+      <section className="h-full bg-cover relative flex items-center pb-52 pt-72 bg-center" style={{
+        backgroundImage:
+          "url(/hero-section.jpg)",
+      }}>
+        <div className="container p-4 z-20 m-auto">
+          <div className="flex flex-col gap-4 w-7/12">
+            <h1>Modern Dental Care for Healthy, Confident Smiles</h1>
+            <p className="text-white">Experience gentle, precise, and patient-focused treatments using advanced dental technology.</p>
+            <div className="flex flex-row gap-2 pt-10">
+              <Link className="button bg-(--color-theme) text-white" href="/">Book an Appointment</Link>
+              <Link className="button border border-white text-white" href="/">Call Us</Link>
+            </div>
+            <small className="text-(--color-light) flex flex-row items-center gap-2"><CalendarClock size={20} /> Open Monday–Saturday. Walk-ins welcome.</small>
+          </div>
+        </div>
+        <div className="absolute inset-0 bg-linear-to-r from-black/95 to-black/40 z-10"></div>
+      </section>
+      <section>
+        <div className="container px-4 py-20 m-auto">
+          <h2>Why Patients Trust Our Dental Care</h2>
+        </div>
+      </section>
+    </main>
   );
 }
