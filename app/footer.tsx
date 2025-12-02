22af45fcf24b5066995cd884d45ba259075ebc5e
Brian Espina <espinabrian@gmail.com>
2025-12-02 17:10:05 +0800

    added footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+import logo from '@/public/healthy-smiles-v2.png'
+
 export default function Footer() {
   return (
-    <footer>
-      <aside></aside>
+    <footer className="p-20 w-full bg-gray-950 border-t-3 border-(--color-accent) text-white text-sm flex items-center gap-10 justify-between">
+      <Image
+        src={logo}
+        width={150}
+        height={100}
+        alt="Dentist Logo"
+      />
+      <small>&copy; 2025 Healthy Smiles Dental Care. All rights reserved.</small>
     </footer>
   )
 }
